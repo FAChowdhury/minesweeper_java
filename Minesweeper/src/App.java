132f830f9857bb3e98db9ecfa54032436b813623
FAChowdhury <chowdhury.farhan235@gmail.com>
2025-02-10 14:54:17 +1100

    started creating board interface

--- Minesweeper/src/App.java
+++ Minesweeper/src/App.java
@@ -1,6 +1,3 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World!");
---
+++ Minesweeper/src/Board/Board.java
@@ -0,0 +1,25 @@
+package Board;
+public interface Board {
+    /**
+     * @return the width of the board.
+     */
+    int getWidth();
+
+    /**
+     * @return the height of the board
+     */
+    int getHeight();
+    
+    /**
+     * Reset the board. This hides all tiles and places new mines.
+     */
+    void reset();
+    
+    /**
+     * Reveals the tile (x, y)
+     * @param x
+     * @param y
+     * @return true if the tile does not contain the mine, otherwise return false.
+     */
+    Boolean revealTile(int x, int y);
+}
